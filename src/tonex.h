6702a473b7f0481645d1396a45f8fdd979815a14
johnnyeatworld89 <johnnyeatworld89@gmail.com>
2026-09-10 11:54:41 +0900

    Update tonex.h

--- src/tonex.h
+++ src/tonex.h
@@ -1,1 +1,21 @@
+#pragma once
 
+struct TonexState
+{
+    uint8_t gain;
+    uint8_t volume;
+
+    uint8_t bass;
+    uint8_t mid;
+    uint8_t treble;
+
+    uint8_t gate;
+    uint8_t compressor;
+    uint8_t reverb;
+
+    uint16_t presetNumber;
+
+    char presetName[32];
+};
+
+extern TonexState tonexState;
