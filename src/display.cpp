c3ebd22983dbb0c0d52455d128e0a51d8364c106
johnnyeatworld89 <johnnyeatworld89@gmail.com>
2026-09-10 12:05:13 +0900

    Update display.cpp

--- src/display.cpp
+++ src/display.cpp
@@ -1,9 +1,16 @@
+#include <Arduino.h>
 #include "display.h"
+#include "tonex.h"
 
 void displayInit()
 {
+    Serial.println("Display Init");
 }
 
 void displayUpdate()
 {
+    Serial.printf("Gain=%d Volume=%d Preset=%s\n",
+                  tonexState.gain,
+                  tonexState.volume,
+                  tonexState.presetName);
 }
